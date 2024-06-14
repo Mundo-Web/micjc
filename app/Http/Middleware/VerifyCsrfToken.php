efd55615a35f0502a3a91643f6dd491a3c82aad1
Carlos Colina <carlosecolina89@gmail.com>
2024-06-14 14:42:46 -0500

    csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'procesar/pago'
+        'procesar/pago',
+        'admin/subcategoria/obtenerd'
     ];
 }
