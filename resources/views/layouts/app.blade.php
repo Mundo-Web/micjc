23ec7d163b10d4b8bf8da14c7698e2dc746cc884
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-07 14:39:05 -0500

    NTS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,9 @@
   <script src="https://cdn.datatables.net/2.0.3/js/dataTables.tailwindcss.js"></script>
   {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" />
+
   <!-- Sweetalert -->
   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
