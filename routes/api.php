c67ecc642df94099413bf7cc038f8824aff9b4ae
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-07 14:40:13 -0500

    MTS

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\SaleController;
+use App\Http\Controllers\SaleDetailController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
