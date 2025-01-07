d634e9ec485325209011f7ff28c2997f48323263
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-07 16:20:49 -0500

    NNTS

--- resources/views/components/sales/table.blade.php
+++ resources/views/components/sales/table.blade.php
@@ -10,13 +10,14 @@
 {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.js"></script>
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery-modal/0.9.1/jquery.modal.min.css" /> --}}
 
-<div id="gridContainer"></div>
+<div id="gridContainer" class="w-full"></div>
 
 <script>
   const salesDataGrid = $('#gridContainer').dxDataGrid({
     language: "es",
     dataSource: {
       load: async (params) => {
+        const estado = document.getElementById("estado")?.value;
         const res = await fetch("{{ route('sales.paginate') }}{{ !$isAdmin ? '?data=mine' : '' }}", {
           method: 'POST',
           headers: {
@@ -26,6 +27,7 @@
           },
           body: JSON.stringify({
             _token: $('[name="_token"]').val(),
+            estado: estado,
             ...params
           })
         })
@@ -56,9 +58,9 @@
     // filterPanel: {
     //   visible: true
     // },
-    // searchPanel: {
-    //   visible: true
-    // },
+    searchPanel: {
+      visible: true
+    },
     // headerFilter: {
     //   visible: true,
     //   search: {
@@ -84,7 +86,7 @@
     //   },
     // },
     paging: {
-      pageSize: 5,
+      pageSize: 10,
     },
     pager: {
       visible: true,
@@ -109,30 +111,49 @@
     //   }
     // },
     columns: [{
-        dataField: 'created_at',
+        dataField: 'name',
+        visible: false,
+      },
+      {
+        dataField: 'lastname',
+        visible: false,
+      },
+      {
+        dataField: 'email',
+        visible: false,
+      },
+      {
+        dataField: 'code',
         caption: 'ORDEN',
+        width: '400px',
         cellTemplate: (container, {
           data
         }) => {
           container.addClass('!px-3 !py-2')
           const div = $('<div>')
           const orderContainer = $('<a>', {
-            class: 'block text-sm font-medium truncate dark:text-white text-blue-500 cursor-pointer max-w-max',
+            class: 'block text-sm font-proppins truncate dark:text-white text-blue-500 cursor-pointer max-w-max',
             text: `#${data.code}`
           })
           if (data.confirmation_client) {
-            orderContainer.append(`<span class="ms-1 inline-flex items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-1 rounded-full dark:bg-green-900 dark:text-green-300 w-max" title="El cliente confirmo la recepcion del producto" tippy>
+            orderContainer.append(`<span class="ms-1 inline-flex items-center bg-green-100 text-green-800 text-xs font-Urbanist_Regular px-2.5 py-1 rounded-full dark:bg-green-900 dark:text-green-300 w-max" title="El cliente confirmo la recepcion del producto" tippy>
               <i class="fa fa-solid fa-user-check"></i>  
             </span>`)
           }
           if (data.confirmation_user) {
-            orderContainer.append(`<span class="ms-1 inline-flex items-center bg-green-100 text-green-800 text-xs font-medium px-2.5 py-1 rounded-full dark:bg-green-900 dark:text-green-300 w-max" title="El vendedor confirmo la entrega del producto" tippy>
+            orderContainer.append(`<span class="ms-1 inline-flex items-center bg-green-100 text-green-800 text-xs font-Urbanist_Regular px-2.5 py-1 rounded-full dark:bg-green-900 dark:text-green-300 w-max" title="El vendedor confirmo la entrega del producto" tippy>
               <i class="fa fa-solid fa-check-to-slot"></i>  
             </span>`)
           }
           tippy('[tippy]', {
             arrow: true
           })
+
+          const nameContainer = $('<p>', {
+            class: 'font-bold',
+            text: `${data.name} ${data.lastname}`
+          });
+
           orderContainer.on('click', () => openSaleModal(data))
           const addressContainer = $('<p>', {
             class: 'text-sm text-gray-500 dark:text-gray-400',
@@ -143,8 +164,15 @@
           const dateContainer = $('<p>', {
             class: 'text-xs text-gray-400',
             text: moment(data.created_at).format('YYYY-MM-DD HH:mm:ss')
-          }).prepend(isAdmin ? `<span class="me-1 text-gray-800">${data.name} ${data.lastname}</span>` : '')
+          })
+
+          const emailContainer = $('<p>', {
+            class: 'text-sm text-blue-500',
+            text: data.email
+          })
           div.append(orderContainer)
+          if (isAdmin) div.append(nameContainer)
+          if (isAdmin) div.append(emailContainer)
           div.append(addressContainer)
           div.append(dateContainer)
 
@@ -152,21 +180,40 @@
         }
       },
       {
+        dataField: 'created_at',
+        caption: 'Fecha',
+        allowFiltering: false,
+        cellTemplate: (container, {
+          data
+        }) => {
+          container.addClass('!px-3 !py-2')
+          const div = $('<div>')
+          const dateContainer = $('<p>', {
+            class: 'text-xs text-gray-400',
+            text: moment(data.created_at).format('YYYY-MM-DD HH:mm:ss')
+          })
+          div.append(dateContainer)
+          container.html(div)
+        }
+      },
+      {
         dataField: 'status.name',
         caption: 'ESTADO',
+        allowFiltering: false,
         cellTemplate: (container, {
           data
         }) => {
           container.addClass('!px-3 !py-2 !text-center')
           container.css('vertical-align', 'middle')
           container.html(
-            `<span class="inline-flex items-center bg-[${data.status?.color ?? '#000000'}77] text-[${data.status?.color ?? '#000000'}30] text-xs font-medium px-2.5 py-0.5 rounded-full dark:bg-[${data.status?.color ?? '#000000'}22] dark:text-[${data.status?.color ?? '#000000'}bb] w-max">${data.status?.name ?? 'Sin estado'}</span>`
+            `<span class="inline-flex items-center bg-[${data.status?.color ?? '#000000'}77] text-white text-sm font-poppins px-2.5 py-0.5 rounded-full dark:bg-[${data.status?.color ?? '#000000'}22] dark:text-[${data.status?.color ?? '#000000'}bb] w-max" style="background-color: ${data.status?.color ?? '#000000'}77; color: white">${data.status?.name ?? 'Sin estado'}</span>`
           )
         }
       },
       {
         dataField: 'total',
         caption: 'MONTO',
+        allowFiltering: false,
         cellTemplate: (container, {
           data
         }) => {
@@ -178,43 +225,22 @@
             class: 'text-center'
           })
           const priceContainer = $('<span>', {
-            class: 'block w-max mx-auto',
+            class: 'block w-max mx-auto font-bold mt-1',
             text: `S/. ${data.total}`
           })
           const envioContainer = $('<span>', {
               class: `inline-flex items-center ${ isFree ? 'bg-green-100' : 'bg-blue-100'} ${ isFree ? 'text-green-800' : 'text-blue-800'} text-xs font-medium px-2.5 py-0.5 rounded-full dark:${ isFree ? 'bg-green-900' : 'bg-blue-900'} dark:${ isFree ? 'text-green-300' : 'text-blue-300'} w-max`
             })
             .append(
-              `<span class="/w-2 h-2 me-1 ${isFree ? 'bg-green-500' : 'bg-blue-500' } rounded-full"></span>`)
-            .append(isFree ? 'Envio gratis' : `S/. ${Number(data.address_price).toFixed(2)}`)
+              `<span class="w-2 h-2 me-1 ${isFree ? 'bg-green-500' : 'bg-blue-500' } rounded-full"></span>`)
+            .append(isFree ? 'Envio gratis' : `Envío: S/. ${Number(data.address_price).toFixed(2)}`)
 
           div.append(priceContainer)
           div.append(envioContainer)
 
           container.html(div)
         }
-      },
-      // {
-      //   dataField: 'tipo_pago',
-      //   caption: 'Tipo de pago',
-      //   lookup: {
-      //     dataSource: [{
-      //         value: 'culqi',
-      //         text: 'Culqi'
-      //       },
-      //       {
-      //         value: 'transferencia',
-      //         text: 'Transferencia'
-      //       },
-      //       {
-      //         value: 'billetera',
-      //         text: 'Yape/Plin'
-      //       }
-      //     ],
-      //     valueExpr: 'value',
-      //     displayExpr: 'text'
-      //   }
-      // }
+      }
     ],
     onContentReady: (...props) => {
       tippy('.tippy-here', {
