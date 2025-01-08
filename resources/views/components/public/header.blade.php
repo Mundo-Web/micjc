7ec476c584a1d407d7358e22c5664c9520cae029
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-08 10:39:15 -0500

    NTS

--- resources/views/components/public/header.blade.php
+++ resources/views/components/public/header.blade.php
@@ -215,6 +215,7 @@
   function deleteItem(id) {
     let articulosCarrito = Local.get('carrito') ?? []
     articulosCarrito = articulosCarrito.filter(objeto => objeto.id != id);
+    console.log(articulosCarrito)
 
     Local.set('carrito', articulosCarrito)
     limpiarHTML()
