99e5111d7792b8871d43e580a33ca33dfc165225
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-08 20:50:30 -0500

    NTS

--- resources/views/public/dashboard_direccion.blade.php
+++ resources/views/public/dashboard_direccion.blade.php
@@ -7,7 +7,11 @@
 
 @section('content')
 
-
+  <style>
+    .select2.select2-container.select2-container--default.select2-container--below {
+      width: 100% !important;
+    }
+  </style>
 
   <main>
     <form id="modal-address" class="!max-w-[600px] !w-full h-max font-Urbanist_Regular max-h-screen relative !opacity-100" style="display: none; padding: 0">
