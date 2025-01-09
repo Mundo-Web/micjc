a876846626cd15e0f1f091e8cd350701a7c24b13
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-08 19:38:27 -0500

    NTS

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Blog;
-use App\Models\Category;
-use App\Models\ClientLogos;
 use App\Models\General;
-use App\Models\Products;
-use App\Models\Tag;
 use App\Models\UserDetails;
 use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Pagination\Paginator as PaginationPaginator;
@@ -45,39 +41,14 @@ class AppServiceProvider extends ServiceProvider
         });
         View::composer('components.public.header', function ($view) {
 
-            $datosgenerales = General::all();
-            $blog = Blog::where('status', '=', 1)->where('visible', '=', 1)->count(); // Suponiendo que tienes un modelo Footer y un método footerData() en él
-            $categoriasMenu = Category::where('visible', '=', 1)->get();
-
-            $categorias = Category::where("status", "=", true)
-            // ->with(['subcategories' => function ($query) {
-            //     $query->whereHas('products');
-            // }])
-            ->get();
-
-            // $marcas = ClientLogos::where('status', true)->where('visible', true)->get();
-
-            $tags = Tag::
-            // where('is_menu', 1)
-            where("status", "=", true)
-            ->where("visible", "=", true)
-            ->whereHas('productos')
-            ->get();
-
-            $offerExists = Products::where('status', true)
-            ->where('descuento', '>', 0)
-                ->exists();
-
-            // Pasar los datos a la vista
-            $view->with([
-                'datosgenerales' => $datosgenerales,
-                'blog' => $blog,
-                'categoriasMenu' => $categoriasMenu,
-                'tags' => $tags,
-                // 'marcas' => $marcas,
-                'offerExists' => $offerExists,
-                'categorias' => $categorias,
-            ]);
+            $user = auth()->user();
+            $userDetail= [];
+            if(isset($user)){
+                $userDetail = UserDetails::where('email', $user->email)->first();
+            }
+            
+            $blogCount =Blog::all()->count();
+            $view->with(['userDetail'=> $userDetail, 'user'=> $user, 'blogCount'=> $blogCount]);
         });
 
 
