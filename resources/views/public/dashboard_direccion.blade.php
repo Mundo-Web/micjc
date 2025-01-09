545978436444146a431d86754510ae48c4841d29
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-08 20:48:51 -0500

    NTS

--- resources/views/public/dashboard_direccion.blade.php
+++ resources/views/public/dashboard_direccion.blade.php
@@ -18,12 +18,12 @@
           cobertura para dicho lugar.</p>
         <div class="flex flex-col gap-4">
           <div class="flex flex-col gap-4 md:flex-row">
-            <div class="basis-1/3 flex flex-col gap-3 z-[45]">
+            <div class="basis-1/3 flex flex-col gap-3 z-[45] w-full">
               <label class="font-medium text-sm text-gray-600">Departamento <span class="text-red-500">*</span></label>
               <div>
                 <div class="dropdown w-full">
                   <select name="departamento_id" id="departamento_id"
-                    class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  pl-3 p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 select2-hidden-accessible"
+                    class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block !w-full  pl-3 p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 select2-hidden-accessible"
                     required>
                     <option value="">- Seleccione -</option>
                     @foreach ($departments as $department)
@@ -34,12 +34,12 @@
               </div>
             </div>
 
-            <div class="basis-1/3 flex flex-col gap-3 z-[40]">
+            <div class="basis-1/3 flex flex-col gap-3 z-[40] w-full">
               <label class="font-medium text-sm text-gray-600">Provincia <span class="text-red-500">*</span></label>
               <div>
                 <div class="dropdown-provincia w-full">
                   <select name="provincia_id" id="provincia_id"
-                    class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full  pl-3 p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 select2-hidden-accessible"
+                    class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block !w-full  pl-3 p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 select2-hidden-accessible"
                     required>
                     <option value="">- Seleccione -</option>
                   </select>
@@ -47,12 +47,12 @@
               </div>
             </div>
 
-            <div class="basis-1/3 flex flex-col gap-3 z-[30]">
+            <div class="basis-1/3 flex flex-col gap-3 z-[30] w-full">
               <label class="font-medium text-sm text-gray-600">Distrito <span class="text-red-500">*</span></label>
               <div>
                 <div class="dropdown-distrito w-full">
                   <select name="distrito_id" id="distrito_id"
-                    class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-3 p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 select2-hidden-accessible"
+                    class="mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block !w-full pl-3 p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 select2-hidden-accessible"
                     required>
                     <option value="">- Seleccione -</option>
                   </select>
