d704a80059e32edc9f8f0ce79ae9e4541cd55eab
gysmoy <gamboapalominocarlosmanuel@gmail.com>
2025-01-15 18:45:58 -0500

    NTS

--- resources/views/components/public/header.blade.php
+++ resources/views/components/public/header.blade.php
@@ -194,13 +194,13 @@
     <div x-data="{ openCatalogo: false, openSubMenu: null }"
       class="bg-[#0051FF] h-10 flex px-[5%] justify-between lg:justify-center lg:gap-10 items-center text-base md:text-lg font-Montserrat_SemiBold">
       <a href="{{ route('index') }}"
-        class=" px-3 py-2 hover:bg-white hover:text-[#0051FF] {{ request()->routeIs('index') ? 'enlaces__after text-white' : 'text-white' }}">Inicio</a>
+        class=" px-4 py-2 hover:bg-white hover:text-[#0051FF] {{ request()->routeIs('index') ? 'enlaces__after text-white' : 'text-white' }}">Inicio</a>
       {{-- <a href="{{ route('catalogo') }}"
         class="{{ request()->routeIs('catalogo') ? 'enlaces__after text-white' : 'text-white' }}">Productos</a> --}}
 
       <nav @mouseenter="openCatalogo = true" @mouseleave="openCatalogo = false" class="block">
         <a href="javascript:void(0)" @click="openCatalogo = true"
-          class="block font-medium px-3 py-2 font-Montserrat_SemiBold text-white hover:bg-white hover:text-[#0051FF] {{ request()->routeIs('catalogo') ? 'enlaces__after text-white' : 'text-white' }}"
+          class="block font-medium px-4 py-2 font-Montserrat_SemiBold text-white hover:bg-white hover:text-[#0051FF] {{ request()->routeIs('catalogo') ? 'enlaces__after text-white' : 'text-white' }}"
           aria-haspopup="true">
           Productos
         </a>
@@ -299,12 +299,12 @@
 
       @if ($blogCount > 0)
         <a href="{{ route('blog', 0) }}"
-          class="px-3 py-2 hover:bg-white hover:text-[#0051FF] {{ request()->routeIs('blog') ? 'enlaces__after text-white' : 'text-white' }}">Blog
+          class="px-4 py-2 hover:bg-white hover:text-[#0051FF] {{ request()->routeIs('blog') ? 'enlaces__after text-white' : 'text-white' }}">Blog
         </a>
       @endif
 
       <a href="{{ route('contacto') }}"
-        class="px-3 py-2 hover:bg-white hover:text-[#0051FF] {{ request()->routeIs('contacto') ? 'enlaces__after text-white' : 'text-white' }}">Contáctanos</a>
+        class="px-4 py-2 hover:bg-white hover:text-[#0051FF] {{ request()->routeIs('contacto') ? 'enlaces__after text-white' : 'text-white' }}">Contáctanos</a>
     </div>
   </div>
 
